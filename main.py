c5a70391b9833d04c32bdd35524d16c728aadd2d
S71D3 <speedman4950@gmail.com>
2020-05-12 17:11:48 +0500

    added exeptions of type + some types

--- main.py
+++ main.py
@@ -20,48 +20,57 @@ class ExampleApp(QtWidgets.QMainWindow, formBaseUi.Ui_MainWindow):
         
     def SqrtOp(self):
         temp = self.askTextBrowser.toPlainText() #забираем строку из первого текстБокса
-        self.answerTextBrowser.setText(SqrtWrk(temp, isMatch(temp), 5)) #отправляем во второй текстовый обработанную строку 
+        self.answerTextBrowser.setText(SqrtWrk(isMatch(temp), 5)) #отправляем во второй текстовый обработанную строку 
                                             #добавить в форму поле с кол-вом знаков для округления и тащить сюда вместо 5
-def isMatch(string):
-    return 1
-    #тут надо проверять соответствие и возвращать какие-то индексы
-    
 
-def SqrtWrk(number, condition, rounder): #тут работаем с самим корнем
+def isMatch(string): #тут надо проверять соответствие и возвращать какие-то индексы
+    pointer = 0
+    try: #пришел int, длинный int, ноль
+        string = int(string)
+        pointer = 1
+    except ValueError:
+        
+        try: #пришел float в виде (1.2; 1.2E+11), длинный float, float-ноль
+            string = locale.atof(string)
+            pointer = 2
+        except ValueError:
+            
+            try: #пришло комплексное число в виде 45+3i или 45+3j
+                string = string.replace('i', 'j') #приводим к перевариваемому питоном виду
+                string = complex(string)
+                pointer = 3
+            except ValueError:
+                string = 'неверный ввод'
+    return [pointer, string]
+
+def SqrtWrk(number, rounder): #тут работаем с самим корнем
     temp = '±' #корни симметричны #надо ли выводить при нуле?
     
     #арифметические +
-    #из нуля +, в двух форматах
+    #из нуля +
     #комплексные
-    #длинные числа +, в двух форматах
-    #заданная точность +, но дописать интерфейс
-    #аналитические
+    #длинные числа +
+    #заданная точность +
+    #аналитические ???????????
     
-    if (condition == 2): 
+    if (number[0] == 1): 
         #пришел int, длинный int, ноль
-        temp += str(round(pow(int(number), 0.5), rounder)) #возводим в степень 0.5, округляем до указанного числа после точки
+        temp += str(round(pow(number[1], 0.5), rounder)) #возводим в степень 0.5, округляем до указанного числа после точки
         #также работает и с длинной арифметикой
         
-    elif (condition == 3): 
+    elif (number[0] == 2): 
         #пришел float в виде (1.2; 1.2E+11), длинный float, float-ноль
-        temp += str(round(pow(locale.atof(number), 0.5), rounder))
+        temp += str(round(pow(number[1], 0.5), rounder))
         
-    elif (condition == 1):
+    elif (number[0] == 3):
         #пришло комплексное число в виде 45+3i или 45+3j
-        number = number.replace('i', 'j') #приводим к перевариваемому питоном виду
-        compTemp = pow(complex(number), 0.5) #находим корень
+        compTemp = pow(number[1], 0.5) #находим корень
         temp += str(round(compTemp.real, rounder) + round(compTemp.imag, rounder)*1j)
         
-    # if (condition == 3):
-        
-    # else:
-    #     temp += str(eval('round(number**0.5,5)')) #5 - точность, надо пофиксить
+    else: #тут можно сделать еще кондишены на ошибки, но оно никому не надо
+        temp = number[1]
+    
     return temp
-#     tfdNum.setText("±"+
-# 			engine.eval("pow("+input+",0.5)").toString());
-#             else
-#                 tfdNum.setText("±" + engine.eval("round(" + input + 
-# 		"**0.5," + spnAccuracy.getValue() + ")").toString()); #кусок обращения к питон-машине у Зимы
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
